302b376b65925adf6dfeb017695e54db93cb2183
dbenbass <dbenbass@gmail.com>
2019-02-06 20:42:11 -0500

    Store hours updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,5 +7,50 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
-})
+ function getStylesheet() {
+       var currentTime = new Date().getHours();
+       if (0 <= currentTime&&currentTime < 9) {
+        $('#morning').hide();
+        $('#night').show();
+         //night.css
+        $('#lunch').hide();
+       }
+       if (9 <= currentTime&&currentTime < 11) {
+        $('#morning').show();
+        $('#night').hide();
+        $('#lunch').hide();
+
+       }
+         //morning.css
+
+      if (11 <= currentTime&&currentTime < 12) {
+        $('#morning').hide();
+        $('#night').hide();
+        $('#lunch').show();      }
+
+         //morning.css
+
+       if (12 <= currentTime&&currentTime < 16) {
+        $('#morning').show();
+        $('#night').hide();
+        $('#lunch').hide();
+
+       //day
+       }
+       if (16 <= currentTime&&currentTime < 22) {
+        $('#morning').hide();
+        $('#night').show();
+        $('#lunch').hide();
+ }
+        //evening
+       if (22 <= currentTime&&currentTime <= 24) {
+        $('#morning').hide();
+        $('#night').show();
+        $('#lunch').hide();
+
+       }
+       //night
+ }
+
+ getStylesheet();
+  })
